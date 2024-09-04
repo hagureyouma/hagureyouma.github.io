0211b9973a0743b5d0c43693b98cd21e6961043e
Mogege Boku <bokumogege@hotmail.co.jp>
2024-09-04 22:40:02 +0900

    a

--- js/hagureyouma.js
+++ js/hagureyouma.js
@@ -1,6 +1,6 @@
 const modal = $('#modal-container');
 const img = modal.find('img');
-$('img.popup').each(function(index) {
+$('.popup img').each(function(index) {
 	$(this).click(function() {
 		img.attr('src', $(this).attr('src'));
 		modal.show();
