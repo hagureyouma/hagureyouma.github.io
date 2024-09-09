13ad48f88873a948142cffcbf191a65380d25726
Mogege Boku <bokumogege@hotmail.co.jp>
2024-09-09 16:13:23 +0900

    a

--- js/hagureyouma.js
+++ js/hagureyouma.js
@@ -1,11 +1,22 @@
-const modal = $('#modal-container');
-const img = modal.find('img');
-$('.popup img').each(function(index) {
-	$(this).click(function() {
-		img.attr('src', $(this).attr('src'));
-		modal.show();
+//画像の拡大表示
+const modal = document.getElementById('modal-container');
+const img = modal.querySelector('img');
+document.querySelectorAll('.popup img').forEach(function (elem) {
+	elem.addEventListener('click', () => {
+		img.src = elem.src;
+		modal.style.display = 'block';
 	})
 });
-modal.click(function() {
-	$(this).hide();
-});
+modal.addEventListener('click', () => modal.style.display = 'none');
+
+//もくじ自動生成
+const nv = document.querySelector('nav');
+const hs = document.querySelectorAll('h2');
+if (hs.length > 1) {
+	let cont='';
+	hs.forEach((h,i)=>{
+		cont+=`<li><a href="#head${i}">${h.textContent}</a></li>`;
+		h.innerHTML+=`<a id="head${i}"></a>`;
+	});
+	nv.innerHTML=`<div class="menu"><h3 class="head">目次</h3><ul>${cont}</ul></div>`;
+}
