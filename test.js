cedccffaa1a80771401bea7793dcb4228032ed23
Mogege Boku <bokumogege@hotmail.co.jp>
2024-09-09 14:41:32 +0900

    a

---
+++ test.js
@@ -0,0 +1,15 @@
+alert("あああ");
+
+// const modal = document.getElementById('modal-container');
+
+// const imgframe = modal.querySelector(img);
+// document.querySelectorAll('.popup img').forEach(function (elem) {
+// 	elem.addEventListener('click', function () {
+// 		imgframe.src = elem.src;
+// 		modal.style.display = 'block';		
+// 	})
+// });
+
+// modal.addEventListener('click', function () {
+// 	modal.style.display = 'none';
+// });
