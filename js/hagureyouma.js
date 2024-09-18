05e455314f7784f964a8daa72351d236950a0091
Mogege Boku <bokumogege@hotmail.co.jp>
2024-09-19 00:01:32 +0900

    a

--- js/hagureyouma.js
+++ js/hagureyouma.js
@@ -1,5 +1,8 @@
 //画像の拡大表示
+const body = document.querySelector('body');
+body.insertAdjacentHTML('afterend', '<div id="modal-container"><div><img src=""></div></div>')
 const modal = document.querySelector('#modal-container');
+modal.addEventListener('click', () => modal.style.display = 'none');
 const img = modal.querySelector('img');
 document.querySelectorAll('.popup img').forEach(function (elem) {
 	elem.addEventListener('click', () => {
@@ -7,7 +10,6 @@ document.querySelectorAll('.popup img').forEach(function (elem) {
 		modal.style.display = 'block';
 	})
 });
-modal.addEventListener('click', () => modal.style.display = 'none');
 
 //もくじ自動生成
 const hs = document.querySelectorAll('h2');
@@ -15,7 +17,7 @@ if (hs.length > 0) {
 	let cont = '';
 	hs.forEach((h, i) => {
 		cont += `<li><a href="#head${i}">${h.textContent}</a></li>`;
-		h.insertAdjacentHTML('afterbegin',`<a id="head${i}"></a>`);
+		h.insertAdjacentHTML('afterbegin', `<a id="head${i}"></a>`);
 	});
 	document.querySelector('nav').innerHTML = `<div class="menu"><h3 class="head">目次</h3><ul>${cont}</ul></div>`;
 }
