3ae804484777d58eb5979b7f47299a723a3b2203
Mogege Boku <bokumogege@hotmail.co.jp>
2024-09-24 22:27:27 +0900

    a

--- js/hagureshooter.js
+++ js/hagureshooter.js
@@ -1,6 +1,12 @@
 'use strict';
 
 const iconUrl = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css';
+
+const nanameCorrect = 0.71;
+
+const playerMoveSpeed = 400;
+const playerBulletRate = 1 / 8;
+
 class Game {
     constructor(width = 320, height = 480) {
         const link = document.createElement('link');
@@ -12,7 +18,7 @@ class Game {
         document.querySelector('body').appendChild(this.canvas);
         this.canvas.width = width;
         this.canvas.height = height;
-        this.objs = [];
+        this.root = new Iremono();
         this.key = {};
         this.input = {};
         this.time;
@@ -21,7 +27,6 @@ class Game {
     Start(init) {
         //todo:preload
         if (init !== undefined) init();
-
         const _keyevent = e => {
             e.preventDefault();
             for (let key in this.key) {
@@ -41,7 +46,6 @@ class Game {
         game.KeyBind('down', 'ArrowDown');
         game.KeyBind('left', 'ArrowLeft');
         game.KeyBind('right', 'ArrowRight');
-
         this.time = performance.now();
         this._tick();
     }
@@ -49,19 +53,15 @@ class Game {
         const now = performance.now();
         this.delta = Math.min((now - this.time) / 1000.0, 1 / 60.0);
         this.time = now;
-        for (let i = 0; i < this.objs.length; i++) {
-            this.objs[i].update(this.delta);
-        }
+        this.root.update(this.delta);
         const ctx = this.canvas.getContext('2d');
         ctx.fillStyle = '#000000';
         ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
-        for (let i = 0; i < this.objs.length; i++) {
-            this.objs[i].draw(this.canvas);
-        }
+        this.root.draw(this.canvas);
         requestAnimationFrame(this._tick.bind(this));
     }
-    Add(obj) {
-        this.objs.push(obj);
+    Add(scene) {
+        this.root.Add(scene);
     }
     KeyBind(name, key) {
         this.key[name] = key;
@@ -71,9 +71,77 @@ class Game {
         return String.fromCharCode(parseInt(code, 16));
     }
 }
-
+class Base {
+    constructor() {
+        this.id = -1;
+        this.isExist = true;
+    }
+}
+class Pos {
+    constructor() {
+        this.x = this.y = 0;
+        this.vx = this.vy = 0;
+    }
+    update(delta) {
+        this.x += this.vx * delta;
+        this.y += this.vy * delta;
+    }
+}
+class Iremono {
+    constructor() {
+        this.base = new Base();
+        this.maker = {};
+        this.objs = [];
+        this.reserveIndexes = {};
+    }
+    setMaker(name, func) {
+        this.maker[name] = func;
+    }
+    put(obj) {
+        if (obj.base.id < 0) return;
+        obj.base.isExist = false;
+        this.reserveIndexes[obj.constructor.name].push(obj.base.id);
+        console.log(`${obj.constructor.name},${obj.base.id}`);
+    }
+    get(name) {
+        let obj;
+        if (!(name in this.reserveIndexes)) this.reserveIndexes[name] = [];
+        if (this.reserveIndexes[name].length === 0) {
+            obj = this.maker[name]();
+            obj.base.id = this.objs.length;
+            this.objs.push(obj);
+        } else {
+            obj = this.objs[this.reserveIndexes[name].pop()];
+        }
+        obj.base.isExist = true;
+        return obj;
+    }
+    Add(obj) {
+        this.objs.push(obj);
+    }
+    update(delta) {
+        this.preUpdate(delta);
+        for (let i = 0; i < this.objs.length; i++) {
+            const obj = this.objs[i];
+            if (!obj.base.isExist) continue;
+            obj.update(delta);
+        }
+        this.postUpdate(delta);
+    }
+    draw(canvas) {
+        for (let i = 0; i < this.objs.length; i++) {
+            const obj = this.objs[i];
+            if (!obj.base.isExist) continue;
+            obj.draw(canvas);
+        }
+    }
+    preUpdate(delta) { }
+    postUpdate(delta) { }
+}
 class Moji {
     constructor(text) {
+        this.base = new Base();
+        this.pos = new Pos();
         this.text = text;
         this.size = 20;
         this.color = '#ffffff';
@@ -81,8 +149,6 @@ class Moji {
         this.weight = 'normal';
         this.baseLine = 'top';
         this.width = this.height = 0;
-        this.x = this.y = 0;
-        this.vx = this.vy = 0;
         this.rotate = 0;
         this._isCenter = false;
         this._isMiddle = false;
@@ -96,9 +162,7 @@ class Moji {
         return this;
     }
     update(delta) {
-        this.onUpdate(delta);
-        this.x += this.vx * delta;
-        this.y += this.vy * delta;
+        this.pos.update(delta);
     }
     draw(canvas) {
         const ctx = game.canvas.getContext('2d');
@@ -108,120 +172,105 @@ class Moji {
         this.width = tm.width;
         this.height = Math.abs(tm.actualBoundingBoxAscent) + Math.abs(tm.actualBoundingBoxDescent);
         var ox = 0, oy = 0;
-        if (this._isCenter) ox = this.width / 2;
-        if (this._isMiddle) oy = this.height / 2;
-        const x = this.x - ox;
-        const y = this.y - oy;
+        if (this._isCenter) ox = this.width * 0.5;
+        if (this._isMiddle) oy = this.height * 0.5;
+        const x = this.pos.x - ox;
+        const y = this.pos.y - oy;
         if (x < -this.width || x > canvas.width) return;
         if (y < -this.height || y > canvas.height) return;
         ctx.fillStyle = this.color;
         ctx.fillText(this.text, x, y);
     }
-    onUpdate() { }
 }
-class Tsubutsubu {
+class Tofu {
     constructor() {
-        this.id = 0;
-        this.IsExist = true;
-        this.size = 0;
+        this.base = new Base();
+        this.pos = new Pos();
+        this.width = 0;
+        this.height = 0;
         this.color = '#ffffff';
-        this.x = this.y = 0;
-        this.vx = this.vy = 0;
-    }
-}
-class TsubutsubuManager {
-    constructor() {
-        this.objs = [];
-        this.reserveIndexes = [];
-        this.range = { left: 0, right: game.canvas.width, top: 0, bottom: game.canvas.height };
-    }
-    put(obj) {
-        obj.IsExist = false;
-        this.reserveIndexes.push(obj.id);
-    }
-    get(x, y, size, color) {
-        let obj;
-        if (this.reserveIndexes.length === 0) {
-            obj = new Tsubutsubu();
-            obj.id = this.objs.length;
-            this.objs.push(obj);
-        } else {
-            obj = this.objs[this.reserveIndexes.pop()];
-        }
-        obj.IsExist = true;
-        obj.size = size;
-        obj.color = color;
-        obj.x = x;
-        obj.y = y;
-        return obj;
     }
     update(delta) {
-        for (let i = 0; i < this.objs.length; i++) {
-            const obj = this.objs[i];
-            if (!obj.IsExist) continue;
-            obj.x += obj.vx * delta;
-            obj.y += obj.vy * delta;
-            const half = obj.size / 2;
-            if (obj.x + half < this.range.left || obj.x - half > this.range.right || obj.y + half < this.range.top || obj.y - half > this.range.bottom) this.put(obj);
-        }
+        this.pos.update(delta);
     }
     draw(canvas) {
         const ctx = canvas.getContext('2d');
-        for (let i = 0; i < this.objs.length; i++) {
-            const obj = this.objs[i];
-            if (!obj.IsExist) continue;
-            const x = obj.x - obj.size / 2;
-            const y = obj.y - obj.size / 2;
-            if (x < -obj.size || x > canvas.width) continue;
-            if (y < -obj.size || y > canvas.height) continue;
-            ctx.fillStyle = obj.color;
-            ctx.fillRect(x, y, obj.size, obj.size);
-        }
+        const x = this.pos.x - this.width * 0.5;
+        const y = this.pos.y - this.height * 0.5;;
+        if (x < -this.width || x > canvas.width) return;
+        if (y < -this.height || y > canvas.height) return;
+        ctx.fillStyle = this.color;
+        ctx.fillRect(x, y, this.width, this.height);
     }
 }
 const game = new Game();
 game.KeyBind('space', ' ');
 
-const playerMoveSpeed = 400;
-const playerBulletRate = 1 / 8;
+const sceneGame = new Iremono();
+game.Add(sceneGame);
 
 const player = new Moji(game.CodeToStr('f6e2'));
+sceneGame.Add(player);
 player.size = 40;
 player.color = '#de858c';
 player.center();
 player.middle();
-player.x = game.canvas.width / 2;
-player.y = game.canvas.height / 2;
-const bullets = new TsubutsubuManager();
-player.bullets = bullets;
+player.pos.x = game.canvas.width * 0.5;
+player.pos.y = game.canvas.height * 0.5;
 player.bulletCooltime = 0;
-player.onUpdate = (delta) => {
-    player.vx = 0;
-    player.vy = 0;
+
+const playerBullets = new Iremono();
+sceneGame.Add(playerBullets);
+playerBullets.setMaker(Tofu.name, () => new Tofu());
+
+const enemies=new Iremono();
+sceneGame.Add(enemies);
+enemies.setMaker(Moji.name, () => new Moji());
+enemies.spawn=()=>{
+
+}
+
+sceneGame.preUpdate = (delta) => {
+    player.pos.vx = 0;
+    player.pos.vy = 0;
     if (game.input.left) {
-        player.vx = -playerMoveSpeed;
+        player.pos.vx = -playerMoveSpeed;
     }
     if (game.input.right) {
-        player.vx = playerMoveSpeed;
+        player.pos.vx = playerMoveSpeed;
     }
     if (game.input.up) {
-        player.vy = -playerMoveSpeed;
+        player.pos.vy = -playerMoveSpeed;
     }
     if (game.input.down) {
-        player.vy = playerMoveSpeed;
+        player.pos.vy = playerMoveSpeed;
     }
-    if (player.vx !== 0 && player.vy !== 0) {
-        player.vx *= 0.71;
-        player.vy *= 0.71;
+    if (player.pos.vx !== 0 && player.pos.vy !== 0) {
+        player.pos.vx *= nanameCorrect;
+        player.pos.vy *= nanameCorrect;
     }
     if (game.input.space) {
         if (player.bulletCooltime < 0) {
             player.bulletCooltime = playerBulletRate;
-            const bullet = player.bullets.get(player.x, player.y-player.height/2, 4, '#ffffff');
-            bullet.vy = -400;
+            const bullet = playerBullets.get(Tofu.name);
+            bullet.width = 4;
+            bullet.height = 4;
+            bullet.color = '#ffffff';
+            bullet.pos.x = player.pos.x;
+            bullet.pos.y = player.pos.y - player.height * 0.5;
+            bullet.pos.vy = -400;
         } else {
             player.bulletCooltime -= 1 * delta;
         }
+    }
+}
+sceneGame.postUpdate = (delta) => {
+    for (let i = 0; i < playerBullets.objs.length; i++) {
+        const obj = playerBullets.objs[i];
+        if (!obj.base.isExist) continue;
+        const halfX = obj.width * 0.5;
+        const halfY = obj.height * 0.5;
+        if (obj.pos.x + halfX < 0 || obj.pos.x - halfX > game.canvas.width || obj.pos.y + halfY < 0 || obj.pos.y - halfY > game.canvas.height) playerBullets.put(obj);
     }
 }
 
@@ -229,27 +278,7 @@ const text2 = new Moji('はぐれシューター');
 text2.size = 30;
 text2.center();
 text2.middle();
-text2.x = game.canvas.width / 2;
-text2.y = game.canvas.height / 2;
-
-game.Add(player);
-game.Add(player.bullets);
-game.Add(text2);
+text2.pos.x = game.canvas.width * 0.5;
+text2.pos.y = game.canvas.height * 0.5;
+sceneGame.Add(text2);
 game.Start();
-
-// const contents = document.querySelector('body');
-// const canvas = document.createElement('canvas');
-// canvas.id = 'gameScreen';
-// canvas.width = 320;
-// canvas.height = 480;
-// contents.appendChild(canvas);
-// const ctx = canvas.getContext('2d');
-// ctx.textBaseline = 'top';
-// ctx.fillStyle = 'blue';
-// ctx.fillRect(0, 0, canvas.width, canvas.height);
-// ctx.fillStyle = 'white';
-// ctx.font = '50px FontAwesome';
-// const iconInt = parseInt("f6e2", 16);
-// const iconStr = String.fromCharCode(iconInt);
-// ctx.fillText(iconStr, 0, 0);
-// console.log('ああああ');
