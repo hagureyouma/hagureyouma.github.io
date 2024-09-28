af9f576c87de1efc45ee422c9a4dc285add7644e
Mogege Boku <bokumogege@hotmail.co.jp>
2024-09-28 21:53:42 +0900

    a

--- js/hagureshooter.js
+++ js/hagureshooter.js
@@ -9,6 +9,7 @@ const emojiCrow = 'f520';
 
 const playerMoveSpeed = 600;
 const playerBulletRate = 1 / 20;
+const baddiesBulletRate = 1 / 1;
 
 class Game {
     constructor(width = 320, height = 480) {
@@ -50,18 +51,18 @@ class Game {
         game.keyBind('left', 'ArrowLeft');
         game.keyBind('right', 'ArrowRight');
         this.time = performance.now();
-        this._tick();
+        this.tick();
     }
-    _tick() {
+    tick() {
         const now = performance.now();
         this.delta = Math.min((now - this.time) / 1000.0, 1 / 60.0);
         this.time = now;
-        this.root._tick(this.delta);
+        this.root.tick();
         const ctx = this.canvas.getContext('2d');
         ctx.fillStyle = '#000000';
         ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
-        this.root.draw(this.canvas);
-        requestAnimationFrame(this._tick.bind(this));
+        this.root.draw();
+        requestAnimationFrame(this.tick.bind(this));
     }
     add(scene) {
         this.root.add(scene);
@@ -117,18 +118,22 @@ function* waitForFrag(func) {
         yield null;
     }
 }
+
 class Base {
     constructor() {
         this.id = -1;
         this.isExist = true;
+        this.putback;
     }
+    update() { }
+    draw() { }
 }
 class Pos {
     constructor() {
         this.x = this.y = 0;
-        this.vx = this.vy = 0;
         this.width = this.height = 0;
         this.isCenter = this.isMiddle = true;
+        this.vx = this.vy = 0;
     }
     update() {
         this.x += this.vx * game.delta;
@@ -136,6 +141,13 @@ class Pos {
     }
     getScreenX = () => (this.x - (Number(this.isCenter) * this.width * 0.5));
     getScreenY = () => (this.y - (Number(this.isMiddle) * this.height * 0.5));
+    isIntersect(other) {
+        const x = this.getScreenX();
+        const y = this.getScreenY();
+        const ox = other.getScreenX();
+        const oy = other.getScreenY();
+        return ox + other.width > x && x + this.width > ox && oy + other.height > y && y + this.height > oy;
+    }
 }
 class Iremono {
     constructor() {
@@ -147,18 +159,16 @@ class Iremono {
     addCreator(name, func, init = undefined) {
         this.maker[name] = { func, init };
     }
-    put(obj) {
-        if (obj.base.id < 0) return;
-        obj.base.isExist = false;
-        this.reserves[obj.constructor.name].push(obj.base.id);
-        console.log(`${obj.constructor.name},${obj.base.id}`);
-    }
     get(name) {
         let obj;
         if (!(name in this.reserves)) this.reserves[name] = [];
         if (this.reserves[name].length === 0) {
             obj = this.maker[name].func();
             obj.base.id = this.objs.length;
+            obj.base.putback = () => {
+                obj.base.isExist = false;
+                this.reserves[name].push(obj.base.id);
+            }
             this.objs.push(obj);
         } else {
             obj = this.objs[this.reserves[name].pop()];
@@ -170,11 +180,11 @@ class Iremono {
     add(obj) {
         this.objs.push(obj);
     }
-    _tick() {
+    tick() {
         this.update();
         for (const obj of this.objs) {
             if (!obj.base.isExist) continue;
-            obj._tick();
+            obj.tick();
         }
         this.postUpdate();
     }
@@ -186,6 +196,12 @@ class Iremono {
     }
     update() { }
     postUpdate() { }
+    each(func) {
+        for (const obj of this.objs) {
+            if (!obj.base.isExist) continue;
+            func(obj);
+        }
+    }
 }
 class Moji {
     constructor(text = '', { size = 20, color = '#ffffff', font = 'FontAwesome', weight = 'normal', isCenter = false, isMiddle = false } = {}) {
@@ -201,7 +217,7 @@ class Moji {
         this.baseLine = 'top';
         this.rotate = 0;
     }
-    _tick() {
+    tick() {
         this.pos.update();
         this.update();
     }
@@ -218,7 +234,7 @@ class Moji {
         ctx.fillStyle = this.color;
         ctx.fillText(this.text, x, y);
     }
-    update(){}
+    update() { }
 }
 class Tofu {
     constructor() {
@@ -226,8 +242,9 @@ class Tofu {
         this.pos = new Pos();
         this.color = '#ffffff';
     }
-    _tick() {
+    tick() {
         this.pos.update();
+        this.update();
     }
     draw() {
         const x = this.pos.getScreenX();
@@ -237,6 +254,7 @@ class Tofu {
         ctx.fillStyle = this.color;
         ctx.fillRect(x, y, this.pos.width, this.pos.height);
     }
+    update() { }
 }
 export const game = new Game();
 game.keyBind('space', ' ');
@@ -254,9 +272,9 @@ class SpawnData {
 }
 const stage1 = [];
 stage1.push(new SpawnData(2, 50, 50, 'obake'));
-stage1.push(new SpawnData(2, 100, 50, 'obake'));
-stage1.push(new SpawnData(3, 150, 50, 'obake'));
-stage1.push(new SpawnData(4, 200, 50, 'obake'));
+// stage1.push(new SpawnData(2, 100, 50, 'obake'));
+// stage1.push(new SpawnData(3, 150, 50, 'obake'));
+// stage1.push(new SpawnData(4, 200, 50, 'obake'));
 
 
 class ScenePlay extends Iremono {
@@ -267,18 +285,17 @@ class ScenePlay extends Iremono {
         this.text.pos.y = game.canvas.height * 0.5;
         this.add(this.text);
         this.player = new Player();
-        this.add(this.player);
         this.add(this.player.bullets);
+        this.add(this.player);
         this.baddies = new Baddies();
-        this.add(this.baddies);
         this.add(this.baddies.bullets);
+        this.add(this.baddies);
         this.fiber = new Fiber();
         this.stage;
         this.stageStart(stage1);
     }
     update = () => {
         this.fiber.update();
-        this.player.preUpdate();
     }
     postUpdate = () => {
         {
@@ -288,10 +305,23 @@ class ScenePlay extends Iremono {
             this.player.pos.y = Util.clamp(halfY, this.player.pos.y, game.canvas.height - halfY);
         }
         {
-            for (const obj of this.player.bullets.objs) {
-                if (!obj.base.isExist) continue;
-                if (game.isOutOfRange(obj.pos.getScreenX(), obj.pos.getScreenY(), obj.pos.width, obj.pos.height)) this.player.bullets.put(obj);
-            }
+            this.player.bullets.each((bullet) => {
+                if (game.isOutOfRange(bullet.pos.getScreenX(), bullet.pos.getScreenY(), bullet.pos.width, bullet.pos.height)) bullet.base.putback();
+                this.baddies.each((baddie) => {
+                    if (!bullet.pos.isIntersect(baddie.pos)) return;
+                    console.log('ぐわぁぁぁ');
+                    bullet.base.putback();
+                    baddie.base.putback();
+                })
+            });
+        }
+        {
+            this.baddies.bullets.each((bullet) => {
+                if (game.isOutOfRange(bullet.pos.getScreenX(), bullet.pos.getScreenY(), bullet.pos.width, bullet.pos.height)) bullet.base.putback();
+                if (!bullet.pos.isIntersect(this.player.pos)) return;
+                console.log('ぎにゃー');
+                bullet.base.putback();
+            })
         }
     }
     stageStart(stage) {
@@ -319,7 +349,7 @@ class Player extends Moji {
         this.bullets = new Iremono();
         this.bullets.addCreator(Tofu.name, () => new Tofu());
     }
-    preUpdate() {
+    update() {
         this.pos.vx = 0;
         this.pos.vy = 0;
         if (game.input.left) {
@@ -370,12 +400,28 @@ class Baddies extends Iremono {
         const bad = this.get(name);
         bad.pos.x = x;
         bad.pos.y = y;
+        bad.bullets = this.bullets;
     }
 }
 class Baddie extends Moji {
     constructor() {
         super('', { size: 40, color: '#999999', isCenter: true, isMiddle: true });
+        this.bullets;
         this.bulletCooltime = 0;
+    }
+    update() {
+        if (this.bulletCooltime < 0) {
+            this.bulletCooltime = baddiesBulletRate;
+            const bullet = this.bullets.get(Tofu.name);
+            bullet.pos.width = 4;
+            bullet.pos.height = 4;
+            bullet.color = '#ffffff';
+            bullet.pos.x = this.pos.x;
+            bullet.pos.y = this.pos.y;
+            bullet.pos.vy = 200;
+        } else {
+            this.bulletCooltime -= 1 * game.delta;
+        }
     }
 }
 game.add(new ScenePlay());
